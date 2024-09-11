653db217914fe6efd1992a00190f4ed163997885
igor <igorcorreatrifilioc@gmail.com>
2024-09-11 17:56:54 -0300

    tela

--- src/main/java/org/example/util/CarregadorAudio.java
+++ src/main/java/org/example/util/CarregadorAudio.java
@@ -1,10 +1,19 @@
 package org.example.util;
+import javax.sound.sampled.AudioSystem;
 import javax.swing.*;
 import java.awt.*;
 import java.io.IOException;
+import java.io.InputStream;
 import java.net.URL;
+import javax.sound.*;
 
 public class CarregadorAudio
 {
+    private void CarregarAudio(String arq)
+    {
+        InputStream audio = CarregadorAudio.class.getResourceAsStream("/"+arq);
+
+    }
+
 
 }
--- src/main/java/org/example/view/TelaLoginPn.java
+++ src/main/java/org/example/view/TelaLoginPn.java
@@ -15,37 +15,84 @@ public class TelaLoginPn extends JPanel
 
     final int LARGURA = 1000;
     final int ALTURA = 800;
+
     private final Font fnt = CarregadorFonte.CarregaFonte("fonts/space_invaders.ttf");
     private final Font fnt2 = CarregadorFonte.CarregaFonte("fonts/MachineStd-Medium.otf");
+
     private final JLabel txtInfo = new JLabel("Insira seus dados de login:");
+    private final JLabel txtNome = new JLabel("Nome:");
+    private final JLabel txtSenha = new JLabel("Senha:");
     private final JLabel txtLog = new JLabel("");
-    Usuario usuario;
+
+    private final JButton btnCAD = new JButton("CADASTRO");
+    private final JButton btnLogin = new JButton("LOGIN");
+    private final JButton conf = new JButton();
+
+    private final JPanel Cad = new JPanel();
+    private final JPanel Login = new JPanel();
+    private final JPanel intPn = new JPanel();
+    private final JPanel selec = new JPanel();
+
     private final JTextField NmUsr = new JTextField();
     private final JTextField psswrd= new JTextField();
-    TelaLoginPn PAINEL = this;
 
+    Usuario usuario;
+    TelaLoginPn PAINEL = this;
 
     public TelaLoginPn()
     {
+        intPn.setEnabled(false);
+        intPn.setVisible(false);
+
         this.setPreferredSize(new Dimension(ALTURA,LARGURA));
-        JPanel intPn = new JPanel();
+        Cad.setPreferredSize(new Dimension(400,400));
+        Cad.setBackground(Color.BLACK);
+        Cad.add(btnCAD);
+        Cad.setVisible(true);
+
+        Login.setPreferredSize(new Dimension(400,400));
+        Login.setBackground(Color.BLACK);
+        Login.add(btnLogin);
+        Login.setVisible(true);
+
+        selec.setPreferredSize(new Dimension(ALTURA, LARGURA));
+        selec.setBackground(Color.BLACK);
+        selec.setLayout(new BoxLayout(selec,BoxLayout.X_AXIS));
+        selec.add(Cad);
+        selec.add(Login);
+        selec.setVisible(true);
+
         JButton conf = new JButton("Confirmar");
         //organiza o painel em um layout de caixas (BoxLayout.Y_AXIS para eixo y e BoxLayout.X_AXIS para eixo x)
         intPn.setLayout(new BoxLayout(intPn,BoxLayout.Y_AXIS));
         intPn.setPreferredSize(new Dimension(1000,250));
         intPn.setBackground(Color.black);
-        intPn.setVisible(true);
+        //intPn.setVisible(true);
 
         this.setPreferredSize(new Dimension(LARGURA,ALTURA));
         this.setBackground(Color.black);
 
+        txtNome.setForeground(Color.WHITE);
+        txtSenha.setForeground(Color.WHITE);
         txtInfo.setForeground(Color.white);
-        txtInfo.setVisible(true);
-        txtInfo.setFont(fnt);
-        txtLog.setVisible(true);
         txtLog.setForeground(Color.white);
-        txtLog.setFont(fnt);
 
+        txtInfo.setFont(fnt);
+        txtLog.setFont(fnt);
+        txtNome.setFont(fnt);
+        txtSenha.setFont(fnt);
+
+        txtInfo.setVisible(true);
+        txtLog.setVisible(true);
+        txtNome.setVisible(true);
+        txtSenha.setVisible(true);
+
+        btnLogin.setFont(fnt);
+        btnLogin.setForeground(Color.WHITE);
+        btnCAD.setForeground(Color.WHITE);
+        btnLogin.setVisible(true);
+        btnCAD.setFont(fnt);
+        btnCAD.setVisible(true);
         conf.setFont(fnt);
         conf.setForeground(Color.white);
         conf.setPreferredSize(new Dimension(150,100));
@@ -68,6 +115,20 @@ public class TelaLoginPn extends JPanel
             }
         });
 
+        btnCAD.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent actionEvent) {
+
+            }
+        });
+        btnLogin.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent actionEvent) {
+                intPn.setVisible(true);
+                intPn.setVisible(true);
+            }
+        });
+
         NmUsr.setPreferredSize(new Dimension(600,100));
         psswrd.setPreferredSize(new Dimension(600,100));
         NmUsr.setFont(NmUsr.getFont().deriveFont(30f));
@@ -80,9 +141,12 @@ public class TelaLoginPn extends JPanel
         psswrd.setVisible(true);
 
         intPn.add(txtInfo);
+        intPn.add(txtNome);
         intPn.add(NmUsr);
+        intPn.add(txtSenha);
         intPn.add(psswrd);
         intPn.add(conf);
+
         this.add(intPn);
         this.add(txtLog);
         this.setVisible(true);
